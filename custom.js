39ae4d100444a5a3af5bd37deba4442c2c042c22
SHUBHAMSRI1103 <shubh1103sri@outlook.com>
2017-03-26 04:36:34 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,16 @@
+$(function() {
+	window.scrollReveal = new scrollReveal();
+	"use strict";
+	
+	// PreLoader
+	$(window).load(function() {
+		$(".loader").fadeOut(400);
+	});
+
+	// Backstretchs
+	$("#header").backstretch("3.jpg");
+	$("#services").backstretch("3.jpg");
+	
+			
+    
+});
